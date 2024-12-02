4854f3d968fb4d8e05946f48108d635be316154e
sazuko07 <sazu07@gmail.com>
2024-12-02 17:07:00 -0600

    crud project latest push

---
+++ MetalBandsAndAwesomeSongs/src/main/java/com/skilldistillery/metalbandsandawesomesongs/entities/MetalBands.java
@@ -0,0 +1,90 @@
+package com.skilldistillery.metalbandsandawesomesongs.entities;
+
+import java.time.Year;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class MetalBands {
+	@Id 
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="Name_Of_Band")
+	private String nameOfBand;
+	
+	@Column(name="Year_Founded")
+	private Year yearFounded;
+	
+	@Column(name="Popular_Song")
+	private String popularSong;
+	
+	@Override
+	public String toString() {
+		return "MetalBands [id=" + id + ", nameOfBand=" + nameOfBand + ", yearFounded=" + yearFounded + ", popularSong="
+				+ popularSong + "]";
+	}
+
+	public MetalBands() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nameOfBand, popularSong, yearFounded);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MetalBands other = (MetalBands) obj;
+		return id == other.id && Objects.equals(nameOfBand, other.nameOfBand)
+				&& Objects.equals(popularSong, other.popularSong) && Objects.equals(yearFounded, other.yearFounded);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNameOfBand() {
+		return nameOfBand;
+	}
+
+	public void setNameOfBand(String nameOfBand) {
+		this.nameOfBand = nameOfBand;
+	}
+
+	public Year getYearFounded() {
+		return yearFounded;
+	}
+
+	public void setYearFounded(Year yearFounded) {
+		this.yearFounded = yearFounded;
+	}
+
+	public String getPopularSong() {
+		return popularSong;
+	}
+
+	public void setPopularSong(String popularSong) {
+		this.popularSong = popularSong;
+	}
+
+
+}
---
+++ MetalBandsAndAwesomeSongs/src/test/java/com/skilldistillery/metalbandsandawesomesongs/entities/MetalBandsTest.java
@@ -0,0 +1,53 @@
+package com.skilldistillery.metalbandsandawesomesongs.entities;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MetalBandsTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private MetalBands MetalBands;
+	
+	
+	@BeforeAll
+	static void setupBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("MetalBands");
+	}
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+		
+	}
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		MetalBands = em.find(MetalBands.class, 1);
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		MetalBands = null;
+		
+	}
+
+	@Test
+	void test_MetalBands_mappings() {
+		assertNotNull(MetalBands);
+		assertEquals(1999, MetalBands.getYearFounded());
+		
+	}
+
+}
+
