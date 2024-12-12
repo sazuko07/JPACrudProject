437c88050d5d820b555feb2c9fb06fefb9f1c282
sazuko07 <sazu07@gmail.com>
2024-12-12 10:21:37 -0600

    crud project latest push

--- BootForMBAAS/src/main/java/com/skilldistillery/bootformbaas/controllers/MetalBandController.java
+++ BootForMBAAS/src/main/java/com/skilldistillery/bootformbaas/controllers/MetalBandController.java
@@ -1,4 +1,5 @@
 package com.skilldistillery.bootformbaas.controllers;
+
 import java.util.List;
 
 import com.skilldistillery.bootformbaas.data.MetalDAO;
@@ -7,23 +8,27 @@ import com.skilldistillery.metalbandsandawesomesongs.entities.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.skilldistillery.metalbandsandawesomesongs.entities.*;
+import com.skilldistillery.metalbandsandawesomesongs.entities.MetalBand;
 
 @Controller
 public class MetalBandController {
 	private final MetalDAO dao;
+
 	public MetalBandController(MetalDAO dao) {
 		this.dao = dao;
 	}
-	@GetMapping(path={ "/", "home.do", "index.do" })
-	   public String index(Model model) {
+
+	@GetMapping(path = { "/", "home.do", "index.do" })
+	public String index(Model model) {
 		List<MetalBand> metalBand = dao.allMetalBands();
 		model.addAttribute("metalBand", metalBand);
-	    return "index"; 
-	   }
+		return "index";
+	}
+
 	@GetMapping("findBandById.do")
 	public ModelAndView getMetalBandById(@RequestParam("id") int id) {
 		ModelAndView mv = new ModelAndView();
@@ -31,56 +36,61 @@ public class MetalBandController {
 		try {
 			metalBand = dao.findBandById(id);
 		} catch (Exception e) {
-		
+
 			e.printStackTrace();
 		}
 		mv.addObject("metalBand", metalBand);
 		mv.setViewName("Show");
 		return mv;
 	}
-	@GetMapping("addNewBand.do")
-	public ModelAndView addNewBand(@RequestParam("id") int id, @RequestParam("Name_Of_Band") String nameOfBand,
-																@RequestParam("Year_Founded") int yearFounded, @RequestParam("Popular_Song") String popularSong) {
-	ModelAndView mv = new ModelAndView();
-	MetalBand metalBand = new MetalBand();
-	try {
-		metalBand = dao.addNewBand(metalBand);
-	} catch (Exception e) {
-		
-		e.printStackTrace();
+
+	@RequestMapping(path = { "addNewBand.do" })
+	public ModelAndView addNewBand(MetalBand addNewBand) {
+		ModelAndView mv = new ModelAndView();
+		MetalBand metalBand = new MetalBand();
+		try {
+			metalBand = dao.addNewBand(metalBand);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		mv.addObject("metalBand", metalBand);
+		mv.setViewName("WEB-INF/addband.jsp");
+		return mv;
 	}
-	mv.addObject("metalBand", metalBand);
-	mv.setViewName("addband");
-	return mv;
-	}
-	
-	
-	@GetMapping("deleteMetalBand.do")
+
+	@RequestMapping(path = { "deleteMetalBand.do" })
 	public ModelAndView deleteMetalBand(@RequestParam("id") int id) {
 		ModelAndView mv = new ModelAndView();
 		MetalBand metalBand = new MetalBand();
 		try {
-		boolean	removeMetalBand = dao.deleteMetalBand(id);
+			boolean removeMetalBand = dao.deleteMetalBand(id);
 		} catch (Exception e) {
-		
+
 			e.printStackTrace();
 		}
+		mv.addObject("metalBand", metalBand);
+		mv.setViewName("WEB-INF/deleteband.jsp");
 		return mv;
 	}
-	@GetMapping("updateBandInfo.do")
-	public ModelAndView updateBandInfo(@RequestParam("id") int id, @RequestParam("Name_Of_Band") String nameOfBand,
-										@RequestParam("Year_Founded") int yearFounded, @RequestParam("Popular_Song") String popularSong) {
-											
-	
-	ModelAndView mv = new ModelAndView();
-	MetalBand metalBand = new MetalBand();
-	try {
-		metalBand = dao.updateBandInfo(metalBand, id);
-	
-	} catch (Exception e) {
-		
-		e.printStackTrace();
+
+	@RequestMapping(path = { "updateBandInfo.do" })
+	public ModelAndView updateBandInfo(MetalBand updateBandInfo) {
+		ModelAndView mv = new ModelAndView();
+		MetalBand metalBand = new MetalBand();
+		try {
+//			metalBand = dao.updateBandInfo(metalBand, id++);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		mv.addObject("metalBand", metalBand);
+		mv.setViewName("WEB-INF/updateband.jsp");
+		return mv;
 	}
-	return mv;
 }
-}
+
+//code snippets 
+//@RequestParam("id") int id, @RequestParam("Name_Of_Band") String nameOfBand,
+//@RequestParam("Year_Founded") int yearFounded, @RequestParam("Popular_Song") String popularSong)
---
+++ BootForMBAAS/src/main/webapp/WEB-INF/deleteband.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Delete A Band</title>
+</head>
+<body>
+<p>You have successfully deleted a band from the database</p>
+</body>
+</html>
